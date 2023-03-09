d23eef6041e82ad041ad9c613b814666e2b45a07
Ariel Villafane <ariel.g.villafane@gmail.com>
2023-03-09 19:17:40 -0300

    working program

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
-import requests
 import lxml.html as html
+import requests
+import os
+import datetime
 
 HOME_URL = 'https://www.lagaceta.com.ar/'
-
 LINK_TO_ARTICLE = '//div[@class="card"]/div[@class="text"]/h2/a[@href]/@href'
 ARTICLE_TITLE = '//h1[@id="spktitle"]/text()'
 ARTICLE_SUMMARY = '//div[@class="articleHead "]/h2[@id="spksumary"]/text()'
 ARTICLE_CONTENT = '//div[@class="articleBody"]/p/text()'
 
-def parse_titles():
+
+def news(link, today):
+    # This function scraps the entire News article each one and saves in a new folder after the date
+    response = requests.get(link)
+    response = response.content.decode('utf-8')
+    response = html.fromstring(response)
+    
+    title = response.xpath(ARTICLE_TITLE)[0]
+    title.replace('\"','')
     try:
-        response = requests.get(HOME_URL)
-        if response.status_code == 200 :
-            home = response.content.decode('utf-8')
-            parsed = html.fromstring(home)
-            cualquiervariable = parsed.xpath(LINK_TO_ARTICLE)
-            for nota in cualquiervariable:
-                print (nota[39::])
-            
-        else:
-            raise ValueError(f'Error: {response.status_code}')
-    except ValueError as ve:
-        print(ve)
+        summary = response.xpath(ARTICLE_SUMMARY)[0]
+    except IndexError:
+        return
+    content = response.xpath(ARTICLE_CONTENT) 
+    with open(f'{today}/{title}.txt','w', encoding='utf-8') as f:
+        f.write(title)
+        f.write('\n')
+        f.write(summary)
+        f.write('\n\n')
+
+        for p in content:
+            f.write(p)
         
 
+def parse_links():
+    #this function retrieves a list of each Link to any article on the newspaper
+    response = requests.get(HOME_URL)
+    response = response.content.decode("utf-8")
+    parsed_response = html.fromstring(response)
+    parsed = parsed_response.xpath(LINK_TO_ARTICLE)
+    today = datetime.datetime.today().strftime('%d - %m - %Y')
+    if not os.path.isdir(today):
+        os.mkdir(today)
+    
+    for link in parsed:
+        news(link,today)
+
+
+    
+
 def run():
-    parse_titles()
+    parse_links()
 
 if __name__=="__main__":
     run()
